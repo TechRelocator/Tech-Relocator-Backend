c3ba7e82e1e4df8d82697f214f0638b47f461da2
Mike Shen <michael.m.shen@gmail.com>
2023-05-19 23:53:51 +0000

    fixed authentication

--- user_profile/permissions.py
+++ user_profile/permissions.py
@@ -1,11 +1,5 @@
 from rest_framework import permissions
 
-
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-
-        # hover over SAFE_METHODS to see which qualify
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        return obj.owner == request.user
+        return obj.username == request.user
