42e62923e63ac9b5fab76a49f6498401c185709f
Mike Shen <michael.m.shen@gmail.com>
2023-05-22 19:09:34 +0000

    added files

--- job_data/views.py
+++ job_data/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
+from .models import Job
+from .serializer import JobSerializer
 
-# Create your views here.
+
+class JobDataList(ListCreateAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_query = self.request.query_params.get('search', None)
+        if search_query:
+            queryset = queryset.filter(title__icontains=search_query)
+        return queryset[:100]
+
+class JobDataDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
