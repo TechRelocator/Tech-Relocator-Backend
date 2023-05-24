de05dcefa09be035472c49e4ab41b01648cf4d82
Mike Shen <michael.m.shen@gmail.com>
2023-05-24 17:32:35 +0000

    added searchable col api

--- col/views.py
+++ col/views.py
@@ -10,24 +10,15 @@ class ColList(ListCreateAPIView):
     def get_queryset(self):
         queryset = super().get_queryset()
 
-        # search by title
-        search_query = self.request.query_params.get('title', None)
+        # search by state
+        search_query_state = self.request.query_params.getlist('state')
 
-        if search_query:
-            queryset = queryset.filter(title__icontains=search_query)
-
-        # search by location
-        search_query = self.request.query_params.get('location', None)
-
-        if search_query:
-            queryset = queryset.filter(location__icontains=search_query)
-
-        # Search by salary in range
-        salary_search = self.request.query_params.get('salary', None)
-        if salary_search:
-            queryset = queryset.filter(
-                Q(salary_low__lte=salary_search) & Q(salary_high__gte=salary_search)
-            )
+        if search_query_state:
+            # Use Q objects to perform OR operation on multiple states
+            filter_query = Q()
+            for state in search_query_state:
+                filter_query |= Q(state__icontains=state)
+            queryset = queryset.filter(filter_query)
 
         return queryset
 
